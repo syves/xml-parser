27d82749a98a0c296672bb9a601cd749e53bde93
Shakrah Yves <7shakrahs@gmail.com>
2018-08-21 15:33:42 -0700

    initial files and parsing functions

---
+++ xml-parser/project.clj
@@ -0,0 +1,9 @@
+(defproject xml-parser "0.1.0-SNAPSHOT"
+  :description "Efficient db insertion from xml"
+  :url ""
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/data.zip "0.1.1"]
+                 [org.clojure/data.xml "0.0.8"]
+                 [proto-repl "0.3.1"]])
---
+++ xml-parser/src/xml_parser/core.clj
@@ -0,0 +1,30 @@
+(ns xml-parser.core)
+
+(require '[clojure.java.io :as io])
+(require '[clojure.xml :as xml]) //data.xml
+(require '[clojure.zip :as zip])
+(require '[clojure.data.zip.xml :as zip-xml])
+
+  (defn meta->map
+  [root]
+  (into {}
+        (for [m (zip-xml/xml-> root :head :meta)]
+          [(keyword (zip-xml/attr m :type))
+           (zip-xml/text m)])))
+
+(defn nzb->map
+  [input]
+  (let [root (-> input
+                 io/input-stream
+                 xml/parse
+                 zip/xml-zip)]
+    {:meta  (meta->map root)
+     :files (mapv file->map (zip-xml/xml-> root :file))}))
+
+
+  (def filepath "/Users/syves/github.com/syves/lambdawerk-backend-test/xml-parser/resources/update-file.xml")
+  (defn zip-str [file]
+    (zip/xml-zip
+      (xml/parse file)))
+  (def updates (zip-str filepath))
+(println updates)
