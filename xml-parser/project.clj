6370261afd48acdfc050fdbdfebb713887e4038f
Shakrah Yves <7shakrahs@gmail.com>
2018-08-28 09:43:09 +0200

    add postgres deps

--- xml-parser/project.clj
+++ xml-parser/project.clj
@@ -4,6 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/java.jdbc "0.7.8"]
+                 [org.postgresql/postgresql "9.4-1201-jdbc41"]
                  [org.clojure/data.zip "0.1.1"]
                  [org.clojure/data.xml "0.0.8"]
                  [proto-repl "0.3.1"]])
