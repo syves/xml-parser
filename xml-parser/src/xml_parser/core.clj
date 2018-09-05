85f29ea16fca3a89a621df13a09eeec2d7576e3e
Shakrah Yves <7shakrahs@gmail.com>
2018-09-05 19:04:10 +0200

    tryiing to extreact from tree

--- xml-parser/src/xml_parser/core.clj
+++ xml-parser/src/xml_parser/core.clj
@@ -3,8 +3,10 @@
 (:require [clojure.java.io :as io]
           [clojure.xml :as xml]
           [clojure.zip :as zip]
-          [clojure.data.xml :refer :all]
-          [clojure.data.zip.xml :as zip-xml]
+          ;[clojure.data.xml :refer :all]
+          [clojure.data.xml :as c-d-xml :refer [parse]]
+          [clojure.data.zip.xml :as c-d-z-xml
+                :refer [xml-> xml1-> attr attr= text]]
           [clojure.pprint :refer [pprint]])
           (:import org.apache.commons.io.input.BOMInputStream
                    org.apache.commons.io.ByteOrderMark
@@ -35,7 +37,7 @@
                ByteOrderMark/UTF_32BE
                ByteOrderMark/UTF_32LE]))
 
-;Note does not strip whitespace 
+;Note does not strip whitespace
 (defn bom-reader
   "removes BOMs"
   [file]
@@ -59,32 +61,47 @@
 ;;which has the keys :tag, :attrs, and :content. and accessor fns tag, attrs, and content.
 (def x (parse bom-free-rdr))
 
+
+;(with-open [b-reader (bom-reader gzip-filepath)]
+;  (->> b-reader
+;      parse
+;    ))
+
 (:tag x)
+(:content (:tag x))
 
 ;;Returns a zipper for xml elements, easily filterable
 
 (defn get-values-from-tree
-  [tree]
-  (map (fn [person]
-         (->> (filter #(= (:tag %) :member)
-              (:content person)
-              (apply str))))
-       (:content tree)))
+  [tree]; this is a map
+  (map (fn [member]
+         (->> x
+              :content
+              (filter #(= (:tag %) :members)
+              (children)
 
-(get-values-from-tree x)
+              ;emit memmber?
+              ;(filter #(= (:content %) ["firstname", "last name", "date-of-birth", "phone"])
+              ;:content
+              (apply str)
+              )))
+              (:content tree)
+       ))
+
+(take 10 (get-values-from-tree x))
+
 
 ;(take 10 (get-values-from-tree new-tree))
-(->> new-tree
+(->> x
      get-values-from-file)
      (take 10));; remove (take 100000) to get the full sequence
 
-;;var to pass around
-(def updates-tree (xml-to-tree filepath))
+
 
 ;;TODO for each child 'member' in root, get its values.. and create a clojure vector for each?
 ;;2. create an sql query for each hug or HoneySql
 (def tree (tree-root filepath))
 
-(->{:tag :root :content [{:tag :member :content
+(->> {:tag :root :content [{:tag :member :content
   ["firstname", "last name", "date-of-birth", "phone"]}] }
-updtes-tree)
+x)
