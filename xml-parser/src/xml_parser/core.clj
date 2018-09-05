7106da8c052cd9b4566bae4b838ed7daf87ad3a5
Shakrah Yves <7shakrahs@gmail.com>
2018-09-05 12:02:35 +0200

    can remove bom

--- xml-parser/src/xml_parser/core.clj
+++ xml-parser/src/xml_parser/core.clj
@@ -7,14 +7,18 @@
           [clojure.data.zip.xml :as zip-xml]
           [clojure.pprint :refer [pprint]])
           (:import org.apache.commons.io.input.BOMInputStream
-                   org.apache.commons.io.ByteOrderMark))
+                   org.apache.commons.io.ByteOrderMark
+                   java.util.zip.GZIPInputStream))
 
 (def base "/Users/syves/github.com/syves/lambdawerk-backend-test/xml-parser/resources/")
 (def gzip-filepath (str base "update-file.xml.gz"))
+(def filepath (str base "update-file.xml"))
 (def test-str (str base "test-str.xml"))
 (def test-dtd (str base "with-dtd.xml"))
 (def wiki "/Users/syves/Downloads/enwiki-latest-abstract.xml.gz")
 (def small-whitespace (str base "small-whitespace.xml"))
+(def with-bom "/Users/syves/github.com/syves/largeFiles/withBom.xml")
+
 ;https://gist.github.com/biggert/6453648
 (def bom-array
   (into-array [ByteOrderMark/UTF_16LE
@@ -23,54 +27,35 @@
                ByteOrderMark/UTF_32BE
                ByteOrderMark/UTF_32LE]))
 
-            ;   BOMInputStream bomIn = new BOMInputStream(in);
-            ;    if (bomIn.hasBOM()) {
-            ;        // has a UTF-8 BOM
-            ;    }
-
 (defn bom-reader
-  "Returns a BOM contextual reader with the proper encoding set (= BOM)"
+  "removes BOM utf-8"
   [file]
     (-> file
+        io/file
         io/input-stream
-        BOMInputStream.
+        GZIPInputStream.
+        ;only skips utf 8
+        (BOMInputStream. false)
         io/reader ))
+
 
 (parse (bom-reader test-dtd))
 (parse (bom-reader gzip-filepath))
+
 (parse (bom-reader wiki))
 (parse (bom-reader small-whitespace))
 
-;maybe I need to remove the bom instead of changing the encoding?
+(parse (bom-reader with-bom))
 
-;returns a buffer reader
-(defn gzip-reader [filename]
-    (-> filename
-        ;java.io.File, passing each arg to as-file
-        io/file
-        ;Attempts to coerce its argument into an open java.io.InputStream.
-        io/input-stream
-        ;Reads text from a character-input stream, buffering characters so as to provide for the efficient reading of characters, arrays, and lines.
-        io/reader))
-
-(def rdr (gzip-reader test-str))
-
-(defn tree [reader](parse reader))
 
 ;;Returns a lazy tree of the xml/element struct-map,
 ;;which has the keys :tag, :attrs, and :content. and accessor fns tag, attrs, and content.
 (:tag (parse (io/reader (io/input-stream (io/file test-dtd)))))
-(parse (io/reader (io/input-stream (io/file gzip-filepath))))
-
-(parse (io/reader (io/input-stream (io/file wiki))))
 
 ;;TODO zip/xml
 
 (def new-tree (parse (io/reader (io/input-stream (io/file test-dtd)))))
 (:tag new-tree)
-
-(defn tree [file]
-    (xml/parse (gzip-reader file)))
 
     ;;Returns a zipper for xml elements, easily filterable
     ;zip/xml-zip
