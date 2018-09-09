7f4a6e9f932a260c367c399e8c5e30a8738bfe0a
Shakrah Yves <7shakrahs@gmail.com>
2018-09-09 17:01:11 +0200

    fix return value on select

--- xml-parser/test/xml_parser/core_test.clj
+++ xml-parser/test/xml_parser/core_test.clj
@@ -3,26 +3,24 @@
             [xml-parser.core :refer :all]
             [clojure.java.jdbc :as jdbc]))
 
-
-
-
 (deftest a-test
 
   (def test-list-map '({:firstname "JIARA", :lastname "HERTZEL", :date-of-birth "1935-06-05", :phone "1111111111"} {:firstname "00226501", :lastname "MCGREWJR", :date-of-birth "1936-02-01", :phone "1111111111"}))
 
   (testing "what does trans-query return with trys."
-
     (is (=
           (trans-query
             test-list-map
             sql-upsert-builder
-            sql-select-contraint-builder))
-            "fooBar"))
+            sql-select-contraint-builder)
+            "fooBar")))
 
-  ;(testing "transaction query with updates/inserts followed by select ;shows success. Can process the entire update.xml file"
-  ;  (is (=
-  ;         (trans-query list-map sql-upsert-builder sql-select-contraint-builder))
-  ;         ""))
+  (testing "transaction query with updates/inserts followed by select ;shows success. Can process the entire update.xml file"
+    (def actual (query list-map sql-upsert-builder))
+    (print actual)
+    (is (= actual
+           (query list-map sql-upsert-builder))
+        "Can process the entire update.xml file"))
 
   (testing "transaction query with updates/inserts followed by select shows success."
     (is (=
@@ -36,7 +34,7 @@
 
     (is (=
           (query test-list-map sql-select-contraint-builder)
-          '(({:fname "JIARA", :lname "HERTZEL", :dob #inst "1935-06-04T23:00:00.000-00:00", :phone "1111111111"}) ({:fname "00226501", :lname "MCGREWJR", :dob #inst "1936-01-31T23:00:00.000-00:00", :phone "1111111111"}))
+          (({:fname "JIARA", :lname "HERTZEL", :dob #inst "1935-06-04T23:00:00.000-00:00", :phone "1111111111"}) ({:fname "00226501", :lname "MCGREWJR", :dob #inst "1936-01-31T23:00:00.000-00:00", :phone "1111111111"}))
           ))
           )
 
