a5ef699d63aebc44178fd63ff411a9c5e4b32cbb
Shakrah Yves <7shakrahs@gmail.com>
2018-09-09 22:05:59 +0200

    fix typo in phone number

--- xml-parser/test/xml_parser/core_test.clj
+++ xml-parser/test/xml_parser/core_test.clj
@@ -12,32 +12,37 @@
 ;This test is mutating the actual db, shoudl learn how to stub
 (deftest test-query!
   (testing "query with small map updates/inserts sql queries updates the db."
+
+    ;record to be updated exists
+    (is (= (jdbc/query db-spec ["SELECT * FROM person WHERE fname='JIARA' and lname='HERTZEL';"])
+           (quote ({:fname "JIARA", :lname "HERTZEL", :dob #inst "1935-06-04T23:00:00.000-00:00", :phone "5859012134"}))
+           ))
+
+    ;record to be created does not exist
+    (is (= (jdbc/query db-spec ["SELECT * FROM person WHERE fname='00226501' and lname='MCGREWJR';"])
+            ()))
+
+    ;updating and inserting returns only exceptions
     (is (=
           (query-runner test-list-map sql-upsert-builder)
           '("caught exception: No results were returned by the query." "caught exception: No results were returned by the query.")))
 
-    (is (=
-          (query-runner test-list-map sql-select-contraint-builder)
-          (quote (({:fname "JIARA", :lname "HERTZEL", :dob #inst "1935-06-04T23:00:00.000-00:00", :phone "9999999999"}) ()))
-          )))
+    ;existing person is updated
+    ;non existant person is created
+    (is (= (query-runner test-list-map sql-select-contraint-builder)
+            (quote (({:fname "JIARA", :lname "HERTZEL", :dob #inst "1935-06-04T23:00:00.000-00:00", :phone "9999999999"}) ({:fname "00226501", :lname "MCGREWJR", :dob #inst "1936-01-31T23:00:00.000-00:00", :phone "9999999999"})))))
 
+    ;restore the state db to state before insert and update
     (try
       (jdbc/query db-spec (sql-upsert-builder {:firstname "JIARA", :lastname "HERTZEL", :date-of-birth "1935-06-05", :phone "5859012134"}))
-    (catch Exception e (str "caught exception: "                     (.getMessage e)))))
+    (catch Exception e (str "caught exception: "                     (.getMessage e))))
+    (try
+      (jdbc/query db-spec ["delete FROM person WHERE fname='00226501' and lname='MCGREWJR';"])
+    (catch Exception e (str "caught exception: "                     (.getMessage e))))
+    ))
 
-;If this completes without GC then I could filter filter and count the result set...print it?
 ;Did not complete after two hours!
 ;(deftest test-query-large
- ;(testing "transaction query with updates/inserts followed by select ;shows success.Can process the entire update.xml file"
-  ;(def actual (query-runner list-map sql-upsert-builder))
- ;(print actual)
-  ;(is (= actual
-  ;       "Can process the entire update.xml file"))))
-
-;I dont think this will ever work because jdbc throws an exception on Successful updates with error :"caught exception: No results were returned by the query."
-;
-;(deftest trans-query-large
-;  (testing "transaction query with updates/inserts followed by select shows success."
-;    (is (=
-;           (trans-query test-list-map sql-upsert-builder s;ql-select-contraint-builder)
-;           ""))))
+; (testing "transaction query with updates/inserts followed by select ;shows success.Can process the entire update.xml file"
+;   (is (= (query-runner list-map sql-upsert-builder)
+;       "Can process the entire update.xml file"))))
