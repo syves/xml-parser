bfb2fdeffce089045472aee0c2b382e5c8eca5f2
Shakrah Yves <7shakrahs@gmail.com>
2018-09-09 15:53:55 +0200

    wip tests

--- xml-parser/test/xml_parser/core_test.clj
+++ xml-parser/test/xml_parser/core_test.clj
@@ -3,14 +3,21 @@
             [xml-parser.core :refer :all]
             [clojure.java.jdbc :as jdbc]))
 
-(def f (trans-query
-          '({:firstname "JIARA", :lastname "HERTZEL", :date-of-birth "1935-06-05", :phone "1111111111"} {:firstname "00226501", :lastname "MCGREWJR", :date-of-birth "1936-02-01", :phone "1111111111"})
-          sql-upsert-builder
-          sql-select-contraint-builder))
-(print f)
+
+
+
 (deftest a-test
 
   (def test-list-map '({:firstname "JIARA", :lastname "HERTZEL", :date-of-birth "1935-06-05", :phone "1111111111"} {:firstname "00226501", :lastname "MCGREWJR", :date-of-birth "1936-02-01", :phone "1111111111"}))
+
+  (testing "what does trans-query return with trys."
+
+    (is (=
+          (trans-query
+            test-list-map
+            sql-upsert-builder
+            sql-select-contraint-builder))
+            "fooBar"))
 
   ;(testing "transaction query with updates/inserts followed by select ;shows success. Can process the entire update.xml file"
   ;  (is (=
@@ -27,10 +34,11 @@
            (query test-list-map sql-upsert-builder))
            '("caught exception: No results were returned by the query." "caught exception: No results were returned by the query." "caught exception: No results were returned by the query."))
 
-           )
-      (is (=
+    (is (=
           (query test-list-map sql-select-contraint-builder)
-          ({:fname "JIARA", :lname "HERTZEL", :dob #inst "1935-06-04T23:00:00.000-00:00", :phone "1111111111"}) ({:fname "00226501", :lname "MCGREWJR", :dob #inst "1936-01-31T23:00:00.000-00:00", :phone "1111111111"})))
+          '(({:fname "JIARA", :lname "HERTZEL", :dob #inst "1935-06-04T23:00:00.000-00:00", :phone "1111111111"}) ({:fname "00226501", :lname "MCGREWJR", :dob #inst "1936-01-31T23:00:00.000-00:00", :phone "1111111111"}))
+          ))
+          )
 
   (testing "sql-select-builder, should return vector of raw sql."
     (is (=
