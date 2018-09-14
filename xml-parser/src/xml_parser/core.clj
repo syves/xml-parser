fd975927d8b606a7086179c07ee241847e400c15
Shakrah Yves <7shakrahs@gmail.com>
2018-09-14 19:23:45 +0200

    remove all functions and tests with string builders

--- xml-parser/src/xml_parser/core.clj
+++ xml-parser/src/xml_parser/core.clj
@@ -1,11 +1,13 @@
 (ns xml-parser.core
-
-(:require [clojure.java.io :as io]
+  (:require [clojure.java.io :as io]
           [clojure.java.jdbc :as jdbc]
           [clojure.data.xml :as c-d-xml :refer [parse]]
           [clojure.data.zip.xml :as c-d-z-xml
                 :refer [xml-> xml1-> attr attr= text]]
-          [clojure.string :as str])
+          [clojure.string :as str]
+          [clj-time.core :as t]
+          [clj-time.format :as f]
+          [clj-time.coerce :as c])
           (:import org.apache.commons.io.input.BOMInputStream
                    org.apache.commons.io.ByteOrderMark
                    java.util.zip.GZIPInputStream
@@ -54,100 +56,7 @@
                               {}
                               (:content member)))))))))
 
-(defn batch-query [big-map query-builder]
-  (map (fn [rec]
-    (query-builder rec))
-        big-map))
-
-(defn singleton-sql-upsert-builder [rec]
-              [(format "UPDATE person SET phone='%s' WHERE fname='%s' AND lname='%s' AND dob='%s' AND phone!='%s';INSERT INTO person(fname, lname, dob, phone) SELECT '%s','%s','%s','%s' WHERE NOT EXISTS (SELECT * FROM person WHERE fname='%s' AND lname='%s' AND dob='%s');"
-              (get rec :phone "")
-              (get rec :firstname "")
-              (get rec :lastname "")
-              (get rec :date-of-birth "")
-              (get rec :phone "")
-              (get rec :firstname "")
-              (get rec :lastname "")
-              (get rec :date-of-birth "")
-              (get rec :phone "")
-              (get rec :firstname "")
-              (get rec :lastname "")
-              (get rec :date-of-birth ""))])
-
-;TODO no idea how this should be formated.
-(defn sql-upsert-builder [rec]
-              (format "UPDATE person SET phone='%s' WHERE fname='%s' AND lname='%s' AND dob='%s' AND phone!='%s';INSERT INTO person(fname, lname, dob, phone) SELECT '%s','%s','%s','%s' WHERE NOT EXISTS (SELECT * FROM person WHERE fname='%s' AND lname='%s' AND dob='%s');"
-              (get rec :phone "")
-              (get rec :firstname "")
-              (get rec :lastname "")
-              (get rec :date-of-birth "")
-              (get rec :phone "")
-              (get rec :firstname "")
-              (get rec :lastname "")
-              (get rec :date-of-birth "")
-              (get rec :phone "")
-              (get rec :firstname "")
-              (get rec :lastname "")
-              (get rec :date-of-birth ""))
-              )
-
-(defn sql-updat-then-insert-builder [rec]
-  [(format "UPDATE person SET phone='%s' WHERE fname='%s' AND lname='%s' AND dob='%s' AND phone!='%s';"
-          (get rec :phone "")
-          (get rec :firstname "")
-          (get rec :lastname "")
-          (get rec :date-of-birth "")
-          (get rec :phone ""))
-
-  (format "INSERT INTO person(fname, lname, dob, phone) SELECT '%s','%s','%s','%s' WHERE NOT EXISTS (SELECT * FROM person WHERE fname='%s' AND lname='%s' AND dob='%s');"
-        (get rec :firstname "")
-        (get rec :lastname "")
-        (get rec :date-of-birth "")
-        (get rec :phone "")
-        (get rec :firstname "")
-        (get rec :lastname "")
-        (get rec :date-of-birth ""))])
-
-(defn singleton-sql-select-builder [rec]
-  [(format "SELECT * FROM person WHERE fname='%s' AND lname='%s' AND dob='%s'AND phone!='%s';"
-            (get rec :firstname "")
-            (get rec :lastname "")
-            (get rec :date-of-birth "")
-            (get rec :phone ""))])
-
-(defn sql-select-builder [rec]
-  (format "SELECT * FROM person WHERE fname='%s' AND lname='%s' AND dob='%s'AND phone!='%s';"
-          (get rec :firstname "")
-          (get rec :lastname "")
-          (get rec :date-of-birth "")
-          (get rec :phone "")))
-
-;TODO to check if records were updated from dict, move to test?
-(defn sql-select-contraint-builder [rec]
-  [(format "SELECT * FROM person WHERE fname='%s' AND lname='%s' AND dob='%s'AND phone='%s';"
-            (get rec :firstname "")
-            (get rec :lastname "")
-            (get rec :date-of-birth "")
-            (get rec :phone ""))])
-
-;TODO trying to emmit results, but syntax is wrong
-(defn query-runner [records string-builder]
-  (map (fn [rec]
-    ;(try
-      ;try return result sert true
-      (jdbc/query db-spec
-                  (string-builder rec)
-                  #{:as-arrays? true}))
-    ;(catch Exception e (str "caught exception: " (.getMessage e)))))
-      records))
-
-;not a transaction!
-(defn batch-query-runner [queries]
-    (try
-      (jdbc/db-do-commands db-spec false queries)
-    (catch Exception e
-      (str "caught exception: " (.getMessage e))))
-  )
+(def custom-formatter (f/formatter "yyyy-MM-dd"))
 
 (defn update-or-insert!
   [db table rec where-clause]
@@ -165,15 +74,16 @@
                     (get rec :phone "")])
         (catch SQLException e (jdbc/print-sql-exception-chain e)))]
       (if (zero? (first result))
-        (try
-          (jdbc/insert! t-con
-                      table
-                      [:fname :lname :dob :phone]
-                      [(get rec :firstname "")
-                      (get rec :lastname "")
-                      (get rec :date-of-birth "")
-                      (get rec :phone "")])
-          (catch SQLException e (jdbc/print-sql-exception-chain e)))
+      (try
+        (jdbc/insert!
+          db-spec
+          :person
+          [:fname :lname :dob :phone]
+          [(get rec :firstname "")
+           (get rec :lastname "")
+           (java.sql.Date. (c/to-long (f/parse custom-formatter (get rec :date-of-birth ""))))
+           (get rec :phone "")])
+        (catch SQLException e (jdbc/print-sql-exception-chain e)))
           result)
       )))
 
--- xml-parser/test/xml_parser/core_test.clj
+++ xml-parser/test/xml_parser/core_test.clj
@@ -2,114 +2,8 @@
   (:require [clojure.test :refer :all]
             [xml-parser.core :refer :all]
             [clojure.java.jdbc :as jdbc]
-            [clojure.string :as str]
-            [clj-time.core :as t]
-            [clj-time.format :as f])
+            [clojure.string :as str])
             (:import java.sql.SQLException))
-
-;builder for a single query
-(comment (deftest test-sql-select-builder
-
-  (def test-list-map '({:firstname "JIARA",
-                        :lastname "HERTZEL",
-                        :date-of-birth "1935-06-05",
-                        :phone "9999999999"}
-                       {:firstname "00226501",
-                        :lastname "MCGREWJR",
-                        :date-of-birth "1936-02-01",
-                        :phone "9999999999"}))
-
-  (testing "sql-select-builder, should return vector of raw sql."
-    (is (=
-          (singleton-sql-select-builder (first (take 1 test-list-map)))
-          ["SELECT * FROM person WHERE fname='JIARA' AND lname='HERTZEL' AND dob='1935-06-05'AND phone!='9999999999';"])))))
-
-;This test is mutating the actual db, shoudl learn how to stub
-(comment (deftest test-query!
-
-  (def test-list-map '({:firstname "JIARA",
-                        :lastname "HERTZEL",
-                        :date-of-birth "1935-06-05",
-                        :phone "9999999999"}
-                       {:firstname "00226501",
-                        :lastname "MCGREWJR",
-                        :date-of-birth "1936-02-01",
-                        :phone "9999999999"}))
-
-  (testing "query with small map updates/inserts sql queries updates the db."
-
-    ;record to be updated exists
-    (is (= (jdbc/query db-spec ["SELECT * FROM person WHERE fname='JIARA' and lname='HERTZEL';"])
-           (quote ({:fname "JIARA", :lname "HERTZEL", :dob #inst "1935-06-04T23:00:00.000-00:00", :phone "5859012134"}))
-           ))
-
-    ;record to be created does not exist
-    (is (= (jdbc/query db-spec ["SELECT * FROM person WHERE fname='00226501' and lname='MCGREWJR';"])
-            ()))
-
-    ;updating and inserting returns only exceptions
-    (is (=
-          (query-runner test-list-map sql-upsert-builder)
-          '("caught exception: No results were returned by the query." "caught exception: No results were returned by the query.")))
-
-    ;existing person is updated
-    ;non existant person is created
-    (is (= (query-runner test-list-map sql-select-contraint-builder)
-            (quote (({:fname "JIARA", :lname "HERTZEL", :dob #inst "1935-06-04T23:00:00.000-00:00", :phone "9999999999"}) ({:fname "00226501", :lname "MCGREWJR", :dob #inst "1936-01-31T23:00:00.000-00:00", :phone "9999999999"})))))
-
-    ;restore the state db to state before insert and update
-    (try
-      (jdbc/query db-spec (sql-upsert-builder {:firstname "JIARA", :lastname "HERTZEL", :date-of-birth "1935-06-05", :phone "5859012134"}))
-    (catch Exception e (str "caught exception: "                     (.getMessage e))))
-    (try
-      (jdbc/query db-spec ["delete FROM person WHERE fname='00226501' and lname='MCGREWJR';"])
-    (catch Exception e (str "caught exception: "                     (.getMessage e))))
-    )))
-
-    ; I may need to run the three tests with a database state cleanup in between? but I think the first 100 are garbage could try with a select?
-
-(comment (deftest query-upsert-runtime!
-      (testing "query with small map updates/inserts time for each query."
-        ;updating and inserting returns only exceptions
-        (is (= (time (query-runner (take 1 list-map) singleton-sql-upsert-builder)) "Elapsed time: 0.0152 msecs"))
-
-        ;this is alot faster than I would have expected, faster than it took for a single update?
-        ;(is (= (time (query-runner (take 100 list-map) sql-upsert-builder))   "Elapsed time: 0.013707 msecs"))
-
-        ;4 times slower than I would expect
-        ;(is (= (time (query-runner (take 10 list-map) sql-upsert-builder)) "Elapsed time: 0.571054 msecs"))
-        (is (= 2 2)))))
-
-;do command expects a semi colon seperated string to contain a single query. my update with insert is interpreted at 2 queries.
-(comment (deftest batch-ddl-do-commands-runtime!
-  ;20 queries
-  (def batch10 (flatten (batch-query (take 10 list-map) sql-updat-then-insert-builder)))
-
-  ;200 queries
-  (def batch100 (flatten (batch-query (take 100 list-map) sql-updat-then-insert-builder)))
-
-  (testing "test time batch query with ddl do commands."
-    ;17 seconds
-    (is (= (time (batch-query-runner batch10))
-          "Elapsed time: 17713.305038 msecs")))
-    ;3.129 minutes
-    (is (= (time (batch-query-runner batch100))
-          "Elapsed time: 187767.486602 msecs"))
-    (is (= 2 2))))
-
-(comment
-  (deftest with-db-conn-runtime!
-  ;20 queries
-  (def batch10 (flatten (batch-query (take 10 list-map) sql-updat-then-insert-builder)))
-
-  ;200 queries
-  (def batch100 (flatten (batch-query (take 100 list-map) sql-updat-then-insert-builder)))
-
-    (testing "test time batch query with db-connection."
-      (is (= (time (batch-query-with-db-con batch10)) "Elapsed time: 9.801417 msecs"))
-      ;(is (= (time (batch-query-with-db-con batch100)) "Elapsed time: 0.244202 msecs"))
-      (is (= 2 2))))
-)
 
 (comment (deftest batch-conditional-transaction!
   (testing "test conditional transaction batch query"
@@ -122,37 +16,6 @@
            :lastname "HERTZEL",
            :date-of-birth "1935-06-05",
            :phone "9999999999"})))
-
-;this works
-  (comment
-    (try
-      (jdbc/update!
-             db-spec
-             :person
-             {:phone (get rec :phone "")}
-             ["fname = ? AND lname = ? AND dob = CAST (? AS DATE) AND phone <> ?"
-              (get rec :firstname "")
-              (get rec :lastname "")
-              (get rec :date-of-birth "")
-              (get rec :phone "")])
-      (catch SQLException e (jdbc/print-sql-exception-chain e))))
-
-(def custom-formatter (f/formatter "yyyy-MM-dd"))
-(f/parse custom-formatter "20100311")
-
-  (try
-    (jdbc/insert!
-      db-spec
-      :person
-      [:fname :lname :dob :phone]
-      [(get rec :firstname "")
-       (get rec :lastname "")
-       ;(java.sql.Date. millisOfDATE) insert millis
-       ;
-       (f/parse custom-formatter (get rec :dob ""))
-       (get rec :phone "")])
-    (catch SQLException e (jdbc/print-sql-exception-chain e)))
-
 
   (testing "test conditional update transaction "
       (is (= (update-or-insert! db-spec :person
