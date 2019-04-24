175374ce77d1f02ec59ab7e9e5137f0a77bcad3b
Shakrah Yves <7shakrahs@gmail.com>
2019-04-24 15:08:03 +0200

    add resource files

---
+++ xml-parser/resources/junk.clj
@@ -0,0 +1,5 @@
+({:firstname "00226501", :lastname "MCGREWJR", :date-of-birth "1936-02-01", :phone "9796740198"} {:firstname "00226501", :lastname "SCHENERLEIN", :date-of-birth "1935-12-10", :phone "5709742596"}
+
+*{:firstname "00226501", :lastname "SUPRISSE", :date-of-birth "2007-05-10", :phone "5873733594"}
+
+ {:firstname "!0045208521", :lastname "BERRIOS ALVAREN", :date-of-birth "1937-02-17", :phone "5515562220"} {:firstname "!0045208521", :lastname "NGOUY", :date-of-birth "1980-11-13", :phone "4801310152"} {:firstname "!0045208521", :lastname "PLACA", :date-of-birth "1952-12-11", :phone "5679012628"} {:firstname "043651", :lastname "FAJARDO ACOSTA", :date-of-birth "1990-12-13", :phone "4147579941"} {:firstname "043651", :lastname "KAPALKO", :date-of-birth "1945-02-01", :phone "7316015162"} {:firstname "043651", :lastname "MAZARIEGOS GOM", :date-of-birth "2010-09-30", :phone "3094268993"} {:firstname "043651", :lastname "NAINSTEIN", :date-of-birth "1933-09-23", :phone "2542956388"})
